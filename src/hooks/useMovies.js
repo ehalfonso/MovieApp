e0438ae38d1d33040af32e3d416b84031e04aff2
ehalfonso <ehalfonso92@gmail.com>
2024-06-19 00:02:13 -0700

    search fixed

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -1,18 +1,19 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { searchMovies } from "../services/movies";
 
 export const useMovies = ({ search }) => {
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const previousSearch = useRef(search);
 
   const getMovies = async () => {
+    if (previousSearch.current === search) return;
     try {
       setLoading(true);
       setError(null);
+      previousSearch.current = search;
       const newMovies = await searchMovies({ search });
-      console.log("========");
-      // console.log(newMovies);
       setMovies(newMovies);
     } catch (error) {
       setError(error.message);
--- src/services/movies.js
+++ src/services/movies.js
@@ -3,7 +3,6 @@ import React from "react";
 const API_KEY = "872d2517";
 export const searchMovies = async ({ search }) => {
   if (search === "") return null;
-  console.log(search);
   try {
     const resp = await fetch(
       `http://www.omdbapi.com/?apikey=${API_KEY}&s=${search}`
@@ -11,7 +10,6 @@ export const searchMovies = async ({ search }) => {
     const data = await resp.json();
     //mapeo de la lista recibida, para romper con el contrato de la api
     const movies = data.Search;
-    console.log(movies);
     return movies?.map((movie) => {
       return {
         id: movie.imdbID,
